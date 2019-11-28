97e7e12e601b633ec42a6e0b5c191c5a7f914bbb
Maarten de Vries <maarten@de-vri.es>
2019-11-28 15:55:35 +0100

    Initial commit.

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use std::io::Write;
+use hyper::Client;
+use abbrws::DigestAuthCache;
+
+#[tokio::main]
+async fn main() {
+	let client = Client::new();
+	let url = "http://192.168.0.5/rw/iosystem/signals/";
+
+	let mut auth = DigestAuthCache::new("Default User".to_owned(), "robotics".to_owned());
+	let make_request = || hyper::Request::get(url).body(Default::default()).unwrap();
+	let response = auth.request(&client, make_request).await.unwrap();
+
+	let body = body(response).await.unwrap();
+	std::io::stdout().write_all(&body).unwrap();
+}
+
+async fn body(response: hyper::Response<hyper::Body>) -> Result<Vec<u8>, hyper::Error> {
+	let mut body = response.into_body();
+	let mut data = Vec::with_capacity(512);
+	while let Some(chunk) = body.next().await {
+		data.extend(chunk?.into_bytes());
+	}
+
+	Ok(data)
+}
