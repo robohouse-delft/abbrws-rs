9880eb8d16bf17ccb8b5352b8518abe0cfd7dcb5
Maarten de Vries <maarten@de-vri.es>
2020-01-17 22:40:54 +0100

    Save and send cookies.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 mod digest_auth_cache;
+use cookie::Cookie;
+use cookie::CookieJar;
 use digest_auth_cache::DigestAuthCache;
 use hyper::body::HttpBody;
 
@@ -8,6 +10,7 @@ pub struct Client<C> {
 	root_url: http::Uri,
 	auth_cache: DigestAuthCache,
 	http_client: hyper::Client<C, hyper::Body>,
+	cookies: cookie::CookieJar,
 }
 
 #[derive(Clone, Debug)]
@@ -23,6 +26,8 @@ pub enum Error {
 	Http(http::Error),
 	Hyper(hyper::Error),
 	Json(serde_json::Error),
+	InvalidHeader(hyper::header::ToStrError),
+	InvalidCookie(cookie::ParseError),
 }
 
 impl<C> Client<C>
@@ -35,6 +40,7 @@ where
 			root_url,
 			http_client,
 			auth_cache: DigestAuthCache::new(user.into(), password.into()),
+			cookies: cookie::CookieJar::new(),
 		})
 	}
 
@@ -59,15 +65,31 @@ where
 	}
 
 	async fn get(&mut self, url: http::Uri) -> Result<serde_json::Value, Error> {
+		// Copy cookies into a list of HeaderValue objects.
+		let cookie_headers : Vec<_> = self.cookies.iter().map(|cookie| {
+			// Unwrap should be fine, we already parsed it from a HeaderValue earlier.
+			let value = format!("{}={}", cookie.name(), cookie.value());
+			hyper::header::HeaderValue::from_str(&value).unwrap()
+		}).collect();
+
+		// Add cookies.
 		let request = move || {
-			hyper::Request::builder()
-				.uri(url.clone())
-				.method(hyper::Method::GET)
-				.body(hyper::Body::empty())
+			let mut request = hyper::Request::get(url.clone()).body(hyper::Body::empty())?;
+			for cookie in &cookie_headers {
+				request.headers_mut().append(hyper::header::COOKIE, cookie.clone());
+			}
+			Ok(request)
 		};
 
 		// Perform request.
 		let response = self.auth_cache.request(&mut self.http_client, request).await?;
+
+		// Parse cookies.
+		let headers = response.headers();
+		for cookie in headers.get_all(hyper::header::SET_COOKIE) {
+			let cookie = cookie.to_str()?.to_string();
+			self.cookies.add(Cookie::parse(cookie)?);
+		}
 
 		let status = response.status();
 		let body = collect_body(response).await?;
@@ -109,6 +131,8 @@ impl std::fmt::Display for Error {
 			Self::Http(e)          => e.fmt(f),
 			Self::Hyper(e)         => e.fmt(f),
 			Self::Json(e)          => e.fmt(f),
+			Self::InvalidHeader(e) => e.fmt(f),
+			Self::InvalidCookie(e) => e.fmt(f),
 		}
 	}
 }
@@ -143,6 +167,18 @@ impl From<hyper::Error> for Error {
 impl From<serde_json::Error> for Error {
 	fn from(other: serde_json::Error) -> Self {
 		Self::Json(other)
+	}
+}
+
+impl From<hyper::header::ToStrError> for Error {
+	fn from(other: hyper::header::ToStrError) -> Self {
+		Self::InvalidHeader(other)
+	}
+}
+
+impl From<cookie::ParseError> for Error {
+	fn from(other: cookie::ParseError) -> Self {
+		Self::InvalidCookie(other)
 	}
 }
 
