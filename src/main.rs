7f39abe494e78b9e829c6a1dc0f7c46f4550fec9
Maarten de Vries <maarten@de-vri.es>
2020-01-17 22:41:50 +0100

    Make main perform two requests.

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,8 @@
 async fn main() {
 	let mut client = abbrws::Client::new(hyper::Client::new(), "192.168.0.5", "Default User", "robotics").unwrap();
 
-	client.login().await.unwrap();
+	client.get_signals().await.unwrap();
+	client.get_signals().await.unwrap();
 
 // 	let mut auth = DigestAuthCache::new("Default User".to_owned(), "robotics".to_owned());
 // 	let make_request = || hyper::Request::get(url).body(Default::default()).unwrap();
