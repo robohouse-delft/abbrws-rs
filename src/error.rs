2803a311e9290678c3e5db5fa21865a26080021d
Maarten de Vries <maarten@de-vri.es>
2020-01-18 02:03:39 +0100

    Add type for remote failure error.

--- src/error.rs
+++ src/error.rs
@@ -4,9 +4,16 @@ pub struct InvalidStatusError {
 	pub body: serde_json::Value,
 }
 
+#[derive(Clone, Debug)]
+pub struct RemoteFailureError {
+	pub code: Option<u32>,
+	pub message: String,
+}
+
 #[derive(Debug)]
 pub enum Error {
 	InvalidStatus(InvalidStatusError),
+	RemoteFailure(RemoteFailureError),
 	InvalidUri(http::uri::InvalidUri),
 	Http(http::Error),
 	Hyper(hyper::Error),
@@ -21,11 +28,22 @@ impl std::fmt::Display for InvalidStatusError {
 	}
 }
 
+impl std::fmt::Display for RemoteFailureError {
+	fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+		if let Some(code) = self.code {
+			write!(f, "server reported error with code 0x{:08X}: {}", code, self.message)
+		} else {
+			write!(f, "server reported error: {}", self.message)
+		}
+	}
+}
+
 impl std::fmt::Display for Error {
 	fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
 		match self {
-			Self::InvalidUri(e)    => e.fmt(f),
 			Self::InvalidStatus(e) => e.fmt(f),
+			Self::RemoteFailure(e) => e.fmt(f),
+			Self::InvalidUri(e)    => e.fmt(f),
 			Self::Http(e)          => e.fmt(f),
 			Self::Hyper(e)         => e.fmt(f),
 			Self::Json(e)          => e.fmt(f),
@@ -36,11 +54,18 @@ impl std::fmt::Display for Error {
 }
 
 impl std::error::Error for InvalidStatusError {}
+impl std::error::Error for RemoteFailureError {}
 impl std::error::Error for Error {}
 
 impl From<InvalidStatusError> for Error {
 	fn from(other: InvalidStatusError) -> Self {
 		Self::InvalidStatus(other)
+	}
+}
+
+impl From<RemoteFailureError> for Error {
+	fn from(other: RemoteFailureError) -> Self {
+		Self::RemoteFailure(other)
 	}
 }
 
